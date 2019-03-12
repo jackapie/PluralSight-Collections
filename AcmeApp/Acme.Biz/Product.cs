278b41c96222a932f226749e97aad22656423721
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-12 17:13:41 +0000

    Added an array for colorOptions to Product constructor; used an array static method and an instance method; used a for and a foreach loop.

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,6 +16,21 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
+            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+
+            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
+
+            colorOptions.SetValue("Blue", 3);
+
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+            }
+
+            foreach (var color in colorOptions)
+            {
+                Console.WriteLine($"The color is {color}");
+            }
         }
         public Product(int productId,
                         string productName,
