3781008f6b5b0ef05c74a34078f1b0816347a4c3
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-13 15:58:44 +0000

    Replaced the colorOptions array with a colorOptions list

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,21 +16,9 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
-            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
-
-            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
-
-            colorOptions.SetValue("Blue", 3);
-
-            for (int i = 0; i < colorOptions.Length; i++)
-            {
-                colorOptions[i] = colorOptions[i].ToLower();
-            }
-
-            foreach (var color in colorOptions)
-            {
-                Console.WriteLine($"The color is {color}");
-            }
+            var colorOptions = new List<string>(){"Red", "Espresso", "White", "Navy"};
+            
+            Console.WriteLine(colorOptions);
         }
         public Product(int productId,
                         string productName,
