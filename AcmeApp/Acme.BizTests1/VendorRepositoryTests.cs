02cf5ff57ae232d0c78a5f77acf1d3a34f935675
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-13 12:36:39 +0000

    Wrote a Generic Method, RetrieveValues<T>(...), in VendorRepository class (and assoc. unit tests)

---
+++ AcmeApp/Acme.BizTests1/VendorRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class VendorRepositoryTests
+    {
+        [TestMethod()]
+        public void RetrieveValueTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = 42;
+
+            //Act
+            var actual = repository.RetrieveValue<int>("Select...", 42);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveValueStringTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = "test";
+
+            //Act
+            var actual = repository.RetrieveValue<string>("Select...", "test");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
