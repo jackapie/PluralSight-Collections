ba5d38edae7699eafae76cc8934999fdf0f2bc66
Jackie Pearce <jackiepearce83@hotmail.com>
2019-04-03 17:43:17 +0100

    Wrote an iterator method and assoc. test

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -58,6 +58,21 @@ namespace Acme.Biz
             return vendors;
         }
 
+        /// <summary>
+        /// Retrieves all of the approved vendors one at a time
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Vendor> RetrieveWithIterator()
+        {
+            //Get the data from the database
+            this.Retrieve();
+
+            foreach(var vendor in vendors)
+            {
+                Console.WriteLine($"Vendor Id is {vendor.VendorId}");
+                yield return vendor;
+            }
+        }
        
         public T RetrieveValue<T>(string sql, T defaultValue)
         {
